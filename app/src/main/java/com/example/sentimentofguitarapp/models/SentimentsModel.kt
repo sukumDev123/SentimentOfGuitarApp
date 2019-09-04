c213163e7573d1e57547cc7ee1dc85fb4d8896cb
kungo000 <kungo2009@hotmail.com>
2019-09-04 23:10:49 +0700

    --

---
+++ app/src/main/java/com/example/sentimentofguitarapp/models/SentimentsModel.kt
@@ -0,0 +1,3 @@
+package com.example.sentimentofguitarapp.models
+
+data class SentimentsModel(val good: Int? , val neutral : Int? , val neg: Int? , val dataPos: List<String>? , val dataNeg: List<String>? , val dataNeutral: List<String>?)
--- app/src/main/java/com/example/sentimentofguitarapp/presenters/ChartSentimentsCal.kt
+++ app/src/main/java/com/example/sentimentofguitarapp/presenters/ChartSentimentsCal.kt
@@ -1,22 +1,14 @@
 package com.example.sentimentofguitarapp.presenters
 
-class ChartSentimentsCal  {
-    var chartSentimentsData: MutableList<Double>
-    constructor(chartSentimentsData : MutableList<Double>){
-        this.chartSentimentsData = chartSentimentsData
-    }
+class ChartSentimentsCal(var chartSentimentsData: MutableList<Double>) {
 
-    fun totalValOfSemtiments(): Double = this.chartSentimentsData.reduce{ sumVal , eachData -> sumVal + eachData }
-    fun sizeOfSentimentType() : Int {
-        return this.chartSentimentsData.size
-    }
-    fun meanOfSizeData(): Any {
-        return  this.chartSentimentsData.reduce { sumVal, eachData -> sumVal + eachData } / this.sizeOfSentimentType()
-    }
+    fun totalValOfSemtiments(): Double = this.chartSentimentsData.reduce { sumVal, eachData -> sumVal + eachData }
+    fun sizeOfSentimentType(): Int = this.chartSentimentsData.size
 
-    fun findMaxVal(): Double?  {
-        return   this.chartSentimentsData.max()!!
-    }
+    fun meanOfSizeData(): Any =
+        this.chartSentimentsData.reduce { sumVal, eachData -> sumVal + eachData } / this.sizeOfSentimentType()
+
+    fun findMaxVal(): Double = this.chartSentimentsData.max()!!
 
     fun findPerSent(): List<Int> = this.chartSentimentsData.map {
         ((it / this.totalValOfSemtiments()) * 100).toInt()
