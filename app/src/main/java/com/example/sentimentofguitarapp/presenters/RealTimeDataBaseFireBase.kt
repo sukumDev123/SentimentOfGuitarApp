cbd1299da082c9e1a13e136913f47cbe08beb819
kungo000 <kungo2009@hotmail.com>
2019-09-04 23:17:23 +0700

    Add 2 overide method of livedata.

--- app/src/main/java/com/example/sentimentofguitarapp/presenters/RealTimeDataBaseFireBase.kt
+++ app/src/main/java/com/example/sentimentofguitarapp/presenters/RealTimeDataBaseFireBase.kt
@@ -6,12 +6,15 @@ import com.example.sentimentofguitarapp.models.SentimentsModel
 import com.google.firebase.database.*
 
 
-class RealTimeDataBaseFireBase(val database: FirebaseDatabase , val myRef : DatabaseReference , val nameReference : String):
-    LiveData<SentimentsModel>() {
+class RealTimeDataBaseFireBase(
+    database: FirebaseDatabase,
+    nameReference: String
+) : LiveData<SentimentsModel>() {
+
     private val TAG = "FIREBASE LOG:"
     private val dbRef = database.getReference(nameReference)
 
-    private val postListener = object : ValueEventListener {
+    private val valueEventListener = object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             // Get Post object and use the values to update the UI
             val sentiments = dataSnapshot.getValue(SentimentsModel::class.java)
@@ -20,7 +23,18 @@ class RealTimeDataBaseFireBase(val database: FirebaseDatabase , val myRef : Data
 
         override fun onCancelled(databaseError: DatabaseError) {
             // Getting Post failed, log a message
-            Log.w(TAG , "loadPost:onCancelled", databaseError.toException())
+            Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
         }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        dbRef.addValueEventListener(valueEventListener)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+
+        dbRef.removeEventListener(valueEventListener)
     }
 }
